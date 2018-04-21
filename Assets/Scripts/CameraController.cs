7e8a0f95b4d5a464f4933a2650cc631ee3743619
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-21 21:56:00 +0200

    update camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private GameObject player;
+    private Vector3 offset;
+
+    private Vector3 originalCameraPosition;
+    private float shakeAmt = 0;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("CameraController");
+        player = GameObject.FindWithTag("Player");
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate () {
+        transform.position = player.transform.position + offset;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        originalCameraPosition = transform.position;
+        shakeAmt = coll.relativeVelocity.magnitude * .0025f;
+        InvokeRepeating("CameraShake", 0, .01f);
+        Invoke("StopShaking", 0.3f);
+
+    }
+
+    void CameraShake()
+    {
+        if(shakeAmt > 0)
+        {
+            float quakeAmt = Random.value * shakeAmt * 2 - shakeAmt;
+            Vector3 pp = transform.position;
+            pp.y += quakeAmt; // can also add to x and/or z
+            transform.position = pp;
+        }
+    }
+
+    void StopShaking()
+    {
+        CancelInvoke("CameraShake");
+        transform.position = originalCameraPosition;
+    }
+}
