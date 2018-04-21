d5a9d4ada9d4da1f6b591a3455629cd2fdba8fc7
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-21 17:53:27 +0200

    add jump trigger to say that the eneemy shoud play

---
+++ Assets/JumpTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour {
+
+  private void OnTriggerEnter2D(Collider2D other) {
+    if(other.gameObject.tag == "EnemyRunner") {
+      other.gameObject.SendMessage("Jump");
+    }
+  }
+}
