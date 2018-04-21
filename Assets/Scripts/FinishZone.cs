79c71e411f24da3b91060ca712e70829dd1464fe
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-21 20:17:15 +0200

    add finish zone to detect en of level

---
+++ Assets/Scripts/FinishZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishZone : MonoBehaviour {
+
+  private void OnTriggerEnter2D(Collider2D other) {
+    if(other.gameObject.tag == "Player") {
+      GameManager.instance.Win();
+    }
+  }
+}
