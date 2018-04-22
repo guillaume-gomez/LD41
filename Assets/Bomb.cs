ac1e69b3aebcc9eec903920b8fa19dfd3fdff84b
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-22 13:36:37 +0200

    Mock bomb

---
+++ Assets/Bomb.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+  //a holder for our Animator
+  //Animator anim;
+  //a public float for the explosion radius public
+  float explodeRadius = 1f;
+
+  // Use this for initialization
+  void Start () {
+    // anim = GetComponent <Animator>();
+  }
+
+  // Update is called once per frame
+  void Update () {
+    //if (anim.GetCurrentAnimatorStateInfo (0).IsName ("bombdead")) {
+      //destroy all the objects in a radius unless they are tagged Player or hand
+      Collider2D[] colliders = Physics2D.OverlapCircleAll (transform.position, explodeRadius);
+      foreach(Collider2D col in colliders) {
+        if (col.tag == "Player" || col.tag == "EnemyRunner")
+        {
+          GameObject gameobjCharacterBase = col.GetComponent<Collider2D>().gameObject;
+          CharacterBase characterBase = gameobjCharacterBase.GetComponent<CharacterBase>();
+          characterBase.Stop();
+          Destroy(this.gameObject);
+        }
+      }
+    //}
+  }
+}
